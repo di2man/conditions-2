0e3785aaee2413a58f5342e0777f8768af1521d1
di2man <153850965+di2man@users.noreply.github.com>
2023-12-23 13:09:51 +0200

    Update task 3.py

--- task 3/task 3.py
+++ task 3/task 3.py
@@ -3,7 +3,7 @@ try:
    user_select = int(input("Enter action:"))
    n1 = 70
    n2 = 7
-   if user_select == +:
+   if user_select == "+":
       print("n1 + n2")
    elif user_select == "-":
           print("n1 - n2")
